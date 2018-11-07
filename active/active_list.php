a0ce34a281c06d46a3d1c59cf71eda1b939dae28
caiweihan <caiweihan@local.host>
2018-11-07 09:32:07 +0800

    1

--- active/active_list.php
+++ active/active_list.php
@@ -1,4 +1,4 @@
 <?php
-$active_base = array(array("start"=>1541196000,"end"=>1541347200,"type"=>3,"faward"=>"skill","v1"=>""),array("start"=>1541368800,"end"=>1541520000,"type"=>4,"faward"=>"skill","v1"=>""),array("start"=>1541541600,"end"=>1541692800,"type"=>1,"faward"=>"skill","v1"=>""));
+$active_base = array(array("start"=>1541368800,"end"=>1541520000,"type"=>4,"faward"=>"skill","v1"=>""),array("start"=>1541541600,"end"=>1541692800,"type"=>1,"faward"=>"skill","v1"=>""),array("start"=>1541714400,"end"=>1541865600,"type"=>5,"faward"=>"hero","v1"=>""));
 $questionIndex=5;
 ?>
