85a71a4b2cff0c598810dcc1e116bf780f63e2bb
caiweihan <caiweihan@local.host>
2018-11-09 18:05:08 +0800

    1

--- version_1/pay/add_diamond.php
+++ version_1/pay/add_diamond.php
@@ -1,10 +1,10 @@
 <?php 
 	$id = $msg->id;
 	$arr = array(
-		'101'=>array('cost'=>6,'diamond'=>60),
-		'102'=>array('cost'=>30,'diamond'=>305),
-		'103'=>array('cost'=>100,'diamond'=>1024),
-		'104'=>array('cost'=>680,'diamond'=>6980),
+		'101'=>array('cost'=>6,'diamond'=>100),
+		'102'=>array('cost'=>30,'diamond'=>520),
+		'103'=>array('cost'=>150,'diamond'=>2650),
+		'104'=>array('cost'=>600,'diamond'=>10650),
 		'105'=>array('cost'=>1,'diamond'=>1)
 	);
 	
